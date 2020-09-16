1a37386ecd25b85b04741a601e6367d86b9fd578
Mamadou <opmadou@gmail.com>
2020-09-17 01:07:16 +0300

    Prod Mode

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 const host = '0.0.0.0';
 var app = express();
-console.log("i'm listen");
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (_req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
